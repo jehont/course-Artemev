a6a585c8f0371ef68e223019207f151a2d78b2db
Artur Artemev <homicide18881@gmail.com>
2020-11-01 16:09:15 +0400

    First laboratory work.

---
+++ Desktop/git/c#/lab/Second/Second/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Second
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество дней: ");
+            string days = Console.ReadLine();
+            int years = Convert.ToInt32(days) / 365 ;
+            int month = (Convert.ToInt32(days) % 365) / 30;
+            Console.WriteLine(days + " Дней  = " + years + " лет(года) и " + month + " мес.");
+        }
+    }
+}
---
+++ Desktop/git/c#/lab/first/first/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("Введите ваше имя: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Здравстуй " + name + " !");
+        }
+    }
+}
